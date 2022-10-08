8497a27373c060b5d233afa8a7640de57a943c9d
Karol Rostkowski <kjrostkowski@gmail.com>
2022-10-08 12:54:26 +0200

    feat: add form container

---
+++ src/OfferCreationForm/OfferCreationForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OfferCreationForm = () => {
+    return (
+        <form>
+            <label>test</label>
+        </form>
+    );
+};
+
+export default OfferCreationForm;
