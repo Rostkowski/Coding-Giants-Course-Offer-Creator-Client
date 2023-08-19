11d79de4cf1f637f7d9fa7edd1ef0ccff1475c86
Karol Rostkowski <kjrostkowski@gmail.com>
2023-08-19 19:25:33 +0200

    feat: test deployment

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,9 +1,14 @@
 pipeline {
-    agent {label "Mikrus"}
+    agent {
+        docker {
+            image 'node:18.17.1-alpine3.18'
+            args '-p 3000:3000'
+        }
+    }
     stages {
-        stage('Hello') {
+        stage('Deliver') { 
             steps {
-                echo "hello from Jenkinsfile"
+                sh './scripts/deploy.sh'
             }
         }
     }
---
+++ scripts/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+npm run build
+set +x
+
+source_dir="./build"
+destination_dir="/var/www/offer-creator"
+
+if [ ! -d "$source_dir" ]; then
+    echo "Source directory not found: $source_dir"
+    exit 1
+fi
+
+if [ ! -d "$destination_dir" ]; then
+    echo "Destination directory not found, creating: $destination_dir"
+    mkdir -p "$destination_dir"
+fi
+
+cp -r "$source_dir"/* "$destination_dir"
+
+echo "Contents of '$source_dir' copied to '$destination_dir'"
