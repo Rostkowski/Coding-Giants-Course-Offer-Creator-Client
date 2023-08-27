6b96afe37716144a791d2229686a5e837b3be4f1
Karol Rostkowski <kjrostkowski@gmail.com>
2023-08-27 07:01:45 +0200

    feat: add checks to bash script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 set -x
+
 npm install
 npm run build
+
+if [ $? -eq 0 ]; then
+    echo "npm build succeeded"
+else
+    echo "npm build failed"
+    exit 1
+fi
+
 set +x
 
 source_dir="./build"
 destination_dir="/var/www/html/offer-creator"
- 
+
 if [ ! -d "$source_dir" ]; then
     echo "Source directory not found: $source_dir"
     exit 1
