1bfaf7de8eb0db58583eb211594ad8ca963917d8
Karol Rostkowski <kjrostkowski@gmail.com>
2023-10-26 19:48:53 +0200

    feat: add additional tests in playwright

--- tests/pageObjects/selectCourseKindPage.ts
+++ tests/pageObjects/selectCourseKindPage.ts
@@ -1,9 +1,32 @@
-import { Page } from "@playwright/test";
+import { Page, request } from "@playwright/test";
 
 export class SelectCourseKindPage {
     private readonly page: Page;
+    courseKinds: string[];
 
     constructor(page: Page) {
         this.page = page;
+    }
+
+    async downloadCourseKinds(currentCountry: string, currentLanguage: string): Promise<void> {
+        try {
+            const context = await request.newContext();
+            const data = await context.get(`https://giganciprogramowaniaformularz.edu.pl/api/Course/courseKindsProgrammingType`, {
+                headers: {
+                    currentCountry: currentCountry,
+                    currentLanguage: currentLanguage,
+                }
+            });
+
+            const response = await data.json();
+
+            const stationaryKinds = await response['stationaryKinds']?.map((kind: any) => kind.kindName) ?? [];
+            const onlineKinds = await response['onlineKinds']?.map((kind: any) => kind.kindName) ?? [];
+
+            this.courseKinds = stationaryKinds.concat(onlineKinds);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+            this.courseKinds = [];
+        }
     }
 }
---
+++ tests/selectCourseKind.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from "@playwright/test";
+import { SelectCourseKindPage } from "./pageObjects/selectCourseKindPage";
+import { EnumSelector } from "./enums/enumSelectors";
+import { SelectCountryPage } from "./pageObjects/selectCountryPage";
+
+test.beforeEach(async ({page}) => {
+    page.goto('/')
+})
+
+test("When clicking on select element with label 'select course kind' a list with available course kinds for Poland should appear. ", async ({page}) => {
+    const selectCountryPage = new SelectCountryPage(page);
+    const selectCourseKindPage = new SelectCourseKindPage(page);
+    
+    await selectCountryPage.selectCountry('Poland');
+    await selectCourseKindPage.downloadCourseKinds("PL", "pl-PL");
+    await expect(page.locator(`${EnumSelector.selectCourseKindSelector} > option`)).toHaveCount(selectCourseKindPage.courseKinds.length+1)
+})
+
+test("When clicking on select element with label 'select course kind' a list with available course kinds for Spain should appear. ", async ({page}) => {
+    const selectCountryPage = new SelectCountryPage(page);
+    const selectCourseKindPage = new SelectCourseKindPage(page);
+    
+    await selectCountryPage.selectCountry('Spain');
+    await selectCourseKindPage.downloadCourseKinds("es", "es-ES");
+    await expect(page.locator(`${EnumSelector.selectCourseKindSelector} > option`)).toHaveCount(selectCourseKindPage.courseKinds.length+1)
+})
